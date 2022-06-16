1a5237781cfaa1499b09e1defa049d74dc71825e
Racheal Pennell <racheal.larimer@gmail.com>
2022-06-15 17:10:13 -0700

    remove experimental files

--- schemas/hyperdriveSchema.js
+++
@@ -1,5 +0,0 @@
-import { ticket } from './documents/ticket';
-import { message } from './objects/message';
-import { slackAuthor } from './objects/slackAuthor';
-
-export const hyperdriveSchema = [ticket, message, slackAuthor];
